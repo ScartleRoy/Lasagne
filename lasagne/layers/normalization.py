beb561e8211e17dda25ba5832ce9abdac98027bd
ScartleRoy <lllyyy7723@gmail.com>
2016-02-24 15:39:31 -0500

    Update normalization.py

--- lasagne/layers/normalization.py
+++ lasagne/layers/normalization.py
@@ -323,8 +323,8 @@ class BatchNormLayer(Layer):
             # use the statistic of all the data in a mini-batch
             input_mean = input.mean((0,1))
             input_inv_std = T.inv(T.sqrt(input.var((0,1)) + self.epsilon))
-            mean = theano.clone(input_mean, share_inputs=False)
-            inv_std = theano.clone(input_inv_std, share_inputs=False)
+            mean = input_mean
+            inv_std = input_inv_std
             beta = 0 if self.beta is None else self.beta
             gamma = 1 if self.gamma is None else self.gamma
             
